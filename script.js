6531850f850cddda6054c1df535b6161cf21ba1f
Robert <crypto_boss@outlook.de>
2025-08-09 19:05:55 +0200

    Fix v11: Stronger background visibility, reduce NFT gallery to 3 cards, fix NFT strip layout

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-/* Noktra Airdrop Script - Version 10 */
-/* Fixed background, positioning, and NFT display issues */
+/* Noktra Airdrop Script - Version 11 */
+/* Fixed background visibility and NFT display issues */
 
 /* Noktra Airdrop behaviour (PL only) */
 
