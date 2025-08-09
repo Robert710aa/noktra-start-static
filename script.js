01e3704e0ef154e0a76e81d3d4dbbe6ff3ca58d9
Robert <crypto_boss@outlook.de>
2025-08-09 22:49:45 +0200

    v26: Update Telegram link to @Noktra_AirDrop

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Noktra Airdrop Script - Version 25 */
+/* Noktra Airdrop Script - Version 26 */
 /* Fixed NFT display - using 3 local PNG files - removed duplicate styles */
 
 /* Noktra Airdrop behaviour (PL only) */
